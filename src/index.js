1c691ebd66330764e0665d3bfc7c1f940464611b
Ubuntu <ubuntu@ip-172-31-9-203.us-east-2.compute.internal>
2017-10-13 01:54:06 +0000

    responsive, better alerts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Layer, Circle, Stage} from 'react-konva';
+import {Layer, Rect, Circle, Stage} from 'react-konva';
+var ReactToastr = require("react-toastr");
+var {ToastContainer} = ReactToastr;
+
+var ToastMessageFactory = React.createFactory(ReactToastr.ToastMessage.animation);
 
 class GridCircle extends React.Component {	
 	// get the circle color for the current status
@@ -20,11 +24,11 @@ class GridCircle extends React.Component {
 			<Circle
 				x={this.props.x}
 				y={this.props.y}
-				width={40}
-				height={40}
+				width={this.props.width}
+				height={this.props.height}
 				fill={this.getColor()}
-				onClick={() => this.props.handleClick(this)}
-				onTouchStart={() => this.props.handleClick(this)}
+				onClick={() => this.props.handleClick(this.props.id)}
+				onTouchStart={() => this.props.handleClick(this.props.id)}
 			/>
 		);
 	}
@@ -33,241 +37,167 @@ class GridCircle extends React.Component {
 class Counter extends React.Component {
 	render() {
 		return (
-			<h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{this.props.count} / 10</h2>
+			<h6>{this.props.count} / 10 Selected</h6>
 		);
 	}
 }
 
 class App extends React.Component {
-	state = {
-		circles: [
-			{ status:'empty', x:1, y:1 },
-			{ status:'empty', x:2, y:1 },
-			{ status:'empty', x:3, y:1 },
-			{ status:'empty', x:4, y:1 },
-			{ status:'empty', x:5, y:1 },
-			{ status:'empty', x:6, y:1 },
-			{ status:'empty', x:7, y:1 },
-			{ status:'theirs', x:8, y:1 },
-			{ status:'theirs', x:9, y:1 },
-			{ status:'theirs', x:10, y:1 },
-			{ status:'empty', x:11, y:1 },
-			{ status:'empty', x:12, y:1 },
-			{ status:'empty', x:13, y:1 },
-			{ status:'empty', x:14, y:1 },
-			{ status:'empty', x:15, y:1 },
-			{ status:'empty', x:16, y:1 },
-			{ status:'empty', x:17, y:1 },
-			{ status:'empty', x:18, y:1 },
-			{ status:'empty', x:19, y:1 },
-			{ status:'empty', x:20, y:1 },
-			{ status:'empty', x:21, y:1 },
-			{ status:'empty', x:22, y:1 },
-			{ status:'empty', x:23, y:1 },
-			{ status:'theirs', x:24, y:1 },
-			{ status:'theirs', x:25, y:1 },
-			{ status:'theirs', x:1, y:2 },
-			{ status:'empty', x:2, y:2 },
-			{ status:'empty', x:3, y:2 },
-			{ status:'empty', x:4, y:2 },
-			{ status:'empty', x:5, y:2 },
-			{ status:'empty', x:6, y:2 },
-			{ status:'theirs', x:7, y:2 },
-			{ status:'empty', x:8, y:2 },
-			{ status:'empty', x:9, y:2 },
-			{ status:'theirs', x:10, y:2 },
-			{ status:'empty', x:11, y:2 },
-			{ status:'empty', x:12, y:2 },
-			{ status:'empty', x:13, y:2 },
-			{ status:'empty', x:14, y:2 },
-			{ status:'empty', x:15, y:2 },
-			{ status:'empty', x:16, y:2 },
-			{ status:'empty', x:17, y:2 },
-			{ status:'empty', x:18, y:2 },
-			{ status:'empty', x:19, y:2 },
-			{ status:'theirs', x:20, y:2 },
-			{ status:'theirs', x:21, y:2 },
-			{ status:'theirs', x:22, y:2 },
-			{ status:'theirs', x:23, y:2 },
-			{ status:'empty', x:24, y:2 },
-			{ status:'empty', x:25, y:2 },
-			{ status:'empty', x:1, y:3 },
-			{ status:'empty', x:2, y:3 },
-			{ status:'empty', x:3, y:3 },
-			{ status:'theirs', x:4, y:3 },
-			{ status:'empty', x:5, y:3 },
-			{ status:'empty', x:6, y:3 },
-			{ status:'empty', x:7, y:3 },
-			{ status:'empty', x:8, y:3 },
-			{ status:'empty', x:9, y:3 },
-			{ status:'theirs', x:10, y:3 },
-			{ status:'empty', x:11, y:3 },
-			{ status:'empty', x:12, y:3 },
-			{ status:'empty', x:13, y:3 },
-			{ status:'theirs', x:14, y:3 },
-			{ status:'empty', x:15, y:3 },
-			{ status:'empty', x:16, y:3 },
-			{ status:'empty', x:17, y:3 },
-			{ status:'empty', x:18, y:3 },
-			{ status:'empty', x:19, y:3 },
-			{ status:'empty', x:20, y:3 },
-			{ status:'empty', x:21, y:3 },
-			{ status:'empty', x:22, y:3 },
-			{ status:'empty', x:23, y:3 },
-			{ status:'empty', x:24, y:3 },
-			{ status:'empty', x:25, y:3 },
-			{ status:'empty', x:1, y:4 },
-			{ status:'empty', x:2, y:4 },
-			{ status:'empty', x:3, y:4 },
-			{ status:'empty', x:4, y:4 },
-			{ status:'empty', x:5, y:4 },
-			{ status:'theirs', x:6, y:4 },
-			{ status:'empty', x:7, y:4 },
-			{ status:'empty', x:8, y:4 },
-			{ status:'empty', x:9, y:4 },
-			{ status:'empty', x:10, y:4 },
-			{ status:'empty', x:11, y:4 },
-			{ status:'empty', x:12, y:4 },
-			{ status:'empty', x:13, y:4 },
-			{ status:'empty', x:14, y:4 },
-			{ status:'theirs', x:15, y:4 },
-			{ status:'theirs', x:16, y:4 },
-			{ status:'empty', x:17, y:4 },
-			{ status:'empty', x:18, y:4 },
-			{ status:'empty', x:19, y:4 },
-			{ status:'empty', x:20, y:4 },
-			{ status:'empty', x:21, y:4 },
-			{ status:'empty', x:22, y:4 },
-			{ status:'empty', x:23, y:4 },
-			{ status:'empty', x:24, y:4 },
-			{ status:'empty', x:25, y:4 },
-			{ status:'empty', x:1, y:5 },
-			{ status:'theirs', x:2, y:5 },
-			{ status:'theirs', x:3, y:5 },
-			{ status:'theirs', x:4, y:5 },
-			{ status:'theirs', x:5, y:5 },
-			{ status:'empty', x:6, y:5 },
-			{ status:'theirs', x:7, y:5 },
-			{ status:'empty', x:8, y:5 },
-			{ status:'empty', x:9, y:5 },
-			{ status:'empty', x:10, y:5 },
-			{ status:'empty', x:11, y:5 },
-			{ status:'empty', x:12, y:5 },
-			{ status:'empty', x:13, y:5 },
-			{ status:'empty', x:14, y:5 },
-			{ status:'empty', x:15, y:5 },
-			{ status:'empty', x:16, y:5 },
-			{ status:'empty', x:17, y:5 },
-			{ status:'empty', x:18, y:5 },
-			{ status:'theirs', x:19, y:5 },
-			{ status:'theirs', x:20, y:5 },
-			{ status:'empty', x:21, y:5 },
-			{ status:'theirs', x:22, y:5 },
-			{ status:'empty', x:23, y:5 },
-			{ status:'empty', x:24, y:5 },
-			{ status:'empty', x:25, y:5 },
-			{ status:'empty', x:1, y:6 },
-			{ status:'empty', x:2, y:6 },
-			{ status:'empty', x:3, y:6 },
-			{ status:'empty', x:4, y:6 },
-			{ status:'theirs', x:5, y:6 },
-			{ status:'empty', x:6, y:6 },
-			{ status:'theirs', x:7, y:6 },
-			{ status:'empty', x:8, y:6 },
-			{ status:'theirs', x:9, y:6 },
-			{ status:'empty', x:10, y:6 },
-			{ status:'empty', x:11, y:6 },
-			{ status:'empty', x:12, y:6 },
-			{ status:'empty', x:13, y:6 },
-			{ status:'theirs', x:14, y:6 },
-			{ status:'theirs', x:15, y:6 },
-			{ status:'empty', x:16, y:6 },
-			{ status:'empty', x:17, y:6 },
-			{ status:'empty', x:18, y:6 },
-			{ status:'empty', x:19, y:6 },
-			{ status:'empty', x:20, y:6 },
-			{ status:'empty', x:21, y:6 },
-			{ status:'empty', x:22, y:6 },
-			{ status:'theirs', x:23, y:6 },
-			{ status:'empty', x:24, y:6 },
-			{ status:'theirs', x:25, y:6 },
-			{ status:'empty', x:1, y:7 },
-			{ status:'empty', x:2, y:7 },
-			{ status:'empty', x:3, y:7 },
-			{ status:'empty', x:4, y:7 },
-			{ status:'empty', x:5, y:7 },
-			{ status:'empty', x:6, y:7 },
-			{ status:'empty', x:7, y:7 },
-			{ status:'empty', x:8, y:7 },
-			{ status:'theirs', x:9, y:7 },
-			{ status:'theirs', x:10, y:7 },
-			{ status:'theirs', x:11, y:7 },
-			{ status:'theirs', x:12, y:7 },
-			{ status:'empty', x:13, y:7 },
-			{ status:'theirs', x:14, y:7 },
-			{ status:'theirs', x:15, y:7 },
-			{ status:'empty', x:16, y:7 },
-			{ status:'empty', x:17, y:7 },
-			{ status:'empty', x:18, y:7 },
-			{ status:'empty', x:19, y:7 },
-			{ status:'empty', x:20, y:7 },
-			{ status:'empty', x:21, y:7 },
-			{ status:'empty', x:22, y:7 },
-			{ status:'empty', x:23, y:7 },
-			{ status:'empty', x:24, y:7 },
-			{ status:'theirs', x:25, y:7 },
-			{ status:'theirs', x:1, y:8 },
-			{ status:'empty', x:2, y:8 },
-			{ status:'empty', x:3, y:8 },
-			{ status:'empty', x:4, y:8 },
-			{ status:'empty', x:5, y:8 },
-			{ status:'empty', x:6, y:8 },
-			{ status:'empty', x:7, y:8 },
-			{ status:'empty', x:8, y:8 },
-			{ status:'empty', x:9, y:8 },
-			{ status:'theirs', x:10, y:8 },
-			{ status:'theirs', x:11, y:8 },
-			{ status:'empty', x:12, y:8 },
-			{ status:'theirs', x:13, y:8 },
-			{ status:'empty', x:14, y:8 },
-			{ status:'empty', x:15, y:8 },
-			{ status:'empty', x:16, y:8 },
-			{ status:'empty', x:17, y:8 },
-			{ status:'empty', x:18, y:8 },
-			{ status:'empty', x:19, y:8 },
-			{ status:'empty', x:20, y:8 },
-			{ status:'empty', x:21, y:8 },
-			{ status:'empty', x:22, y:8 },
-			{ status:'theirs', x:23, y:8 },
-			{ status:'theirs', x:24, y:8 },
-			{ status:'empty', x:25, y:8 },
-			{ status:'theirs', x:1, y:9 },
-			{ status:'empty', x:2, y:9 },
-			{ status:'empty', x:3, y:9 },
-			{ status:'empty', x:4, y:9 },
-			{ status:'empty', x:5, y:9 },
-			{ status:'empty', x:6, y:9 },
-			{ status:'empty', x:7, y:9 },
-			{ status:'empty', x:8, y:9 },
-			{ status:'theirs', x:9, y:9 },
-			{ status:'theirs', x:10, y:9 },
-			{ status:'theirs', x:11, y:9 },
-			{ status:'empty', x:12, y:9 },
-			{ status:'empty', x:13, y:9 },
-			{ status:'empty', x:14, y:9 },
-			{ status:'empty', x:15, y:9 },
-			{ status:'empty', x:16, y:9 },
-			{ status:'empty', x:17, y:9 },
-			{ status:'empty', x:18, y:9 },
-			{ status:'empty', x:19, y:9 },
-			{ status:'theirs', x:20, y:9 },
-			{ status:'empty', x:21, y:9 },
-			{ status:'empty', x:22, y:9 },
-			{ status:'empty', x:23, y:9 },
-			{ status:'empty', x:24, y:9 },
-			{ status:'empty', x:25, y:9 },
-		]
+	state = {};
+	
+	constructor() {
+		super();
+		
+		this.state = {
+			canvas: this.getCanvas(),
+			circles: [
+				{ status:'empty', x:1, y:1 },
+				{ status:'empty', x:2, y:1 },
+				{ status:'empty', x:3, y:1 },
+				{ status:'empty', x:4, y:1 },
+				{ status:'empty', x:5, y:1 },
+				{ status:'empty', x:6, y:1 },
+				{ status:'empty', x:7, y:1 },
+				{ status:'theirs', x:8, y:1 },
+				{ status:'theirs', x:9, y:1 },
+				{ status:'theirs', x:10, y:1 },
+				{ status:'empty', x:11, y:1 },
+				{ status:'empty', x:12, y:1 },
+				{ status:'empty', x:13, y:1 },
+				{ status:'empty', x:14, y:1 },
+				{ status:'empty', x:15, y:1 },
+				{ status:'theirs', x:1, y:2 },
+				{ status:'empty', x:2, y:2 },
+				{ status:'empty', x:3, y:2 },
+				{ status:'empty', x:4, y:2 },
+				{ status:'empty', x:5, y:2 },
+				{ status:'empty', x:6, y:2 },
+				{ status:'theirs', x:7, y:2 },
+				{ status:'empty', x:8, y:2 },
+				{ status:'empty', x:9, y:2 },
+				{ status:'theirs', x:10, y:2 },
+				{ status:'empty', x:11, y:2 },
+				{ status:'empty', x:12, y:2 },
+				{ status:'empty', x:13, y:2 },
+				{ status:'empty', x:14, y:2 },
+				{ status:'empty', x:15, y:2 },
+				{ status:'empty', x:1, y:3 },
+				{ status:'empty', x:2, y:3 },
+				{ status:'empty', x:3, y:3 },
+				{ status:'theirs', x:4, y:3 },
+				{ status:'empty', x:5, y:3 },
+				{ status:'empty', x:6, y:3 },
+				{ status:'empty', x:7, y:3 },
+				{ status:'empty', x:8, y:3 },
+				{ status:'empty', x:9, y:3 },
+				{ status:'theirs', x:10, y:3 },
+				{ status:'empty', x:11, y:3 },
+				{ status:'empty', x:12, y:3 },
+				{ status:'empty', x:13, y:3 },
+				{ status:'theirs', x:14, y:3 },
+				{ status:'empty', x:15, y:3 },
+				{ status:'empty', x:1, y:4 },
+				{ status:'empty', x:2, y:4 },
+				{ status:'empty', x:3, y:4 },
+				{ status:'empty', x:4, y:4 },
+				{ status:'empty', x:5, y:4 },
+				{ status:'theirs', x:6, y:4 },
+				{ status:'empty', x:7, y:4 },
+				{ status:'empty', x:8, y:4 },
+				{ status:'empty', x:9, y:4 },
+				{ status:'empty', x:10, y:4 },
+				{ status:'empty', x:11, y:4 },
+				{ status:'empty', x:12, y:4 },
+				{ status:'empty', x:13, y:4 },
+				{ status:'empty', x:14, y:4 },
+				{ status:'theirs', x:15, y:4 },
+				{ status:'empty', x:1, y:5 },
+				{ status:'theirs', x:2, y:5 },
+				{ status:'theirs', x:3, y:5 },
+				{ status:'theirs', x:4, y:5 },
+				{ status:'theirs', x:5, y:5 },
+				{ status:'empty', x:6, y:5 },
+				{ status:'theirs', x:7, y:5 },
+				{ status:'empty', x:8, y:5 },
+				{ status:'empty', x:9, y:5 },
+				{ status:'empty', x:10, y:5 },
+				{ status:'empty', x:11, y:5 },
+				{ status:'empty', x:12, y:5 },
+				{ status:'empty', x:13, y:5 },
+				{ status:'empty', x:14, y:5 },
+				{ status:'empty', x:15, y:5 },
+				{ status:'empty', x:1, y:6 },
+				{ status:'empty', x:2, y:6 },
+				{ status:'empty', x:3, y:6 },
+				{ status:'empty', x:4, y:6 },
+				{ status:'theirs', x:5, y:6 },
+				{ status:'empty', x:6, y:6 },
+				{ status:'theirs', x:7, y:6 },
+				{ status:'empty', x:8, y:6 },
+				{ status:'theirs', x:9, y:6 },
+				{ status:'empty', x:10, y:6 },
+				{ status:'empty', x:11, y:6 },
+				{ status:'empty', x:12, y:6 },
+				{ status:'empty', x:13, y:6 },
+				{ status:'theirs', x:14, y:6 },
+				{ status:'theirs', x:15, y:6 },
+				{ status:'empty', x:1, y:7 },
+				{ status:'empty', x:2, y:7 },
+				{ status:'empty', x:3, y:7 },
+				{ status:'empty', x:4, y:7 },
+				{ status:'empty', x:5, y:7 },
+				{ status:'empty', x:6, y:7 },
+				{ status:'empty', x:7, y:7 },
+				{ status:'empty', x:8, y:7 },
+				{ status:'theirs', x:9, y:7 },
+				{ status:'theirs', x:10, y:7 },
+				{ status:'theirs', x:11, y:7 },
+				{ status:'theirs', x:12, y:7 },
+				{ status:'empty', x:13, y:7 },
+				{ status:'theirs', x:14, y:7 },
+				{ status:'theirs', x:15, y:7 },
+				{ status:'theirs', x:1, y:8 },
+				{ status:'empty', x:2, y:8 },
+				{ status:'empty', x:3, y:8 },
+				{ status:'empty', x:4, y:8 },
+				{ status:'empty', x:5, y:8 },
+				{ status:'empty', x:6, y:8 },
+				{ status:'empty', x:7, y:8 },
+				{ status:'empty', x:8, y:8 },
+				{ status:'empty', x:9, y:8 },
+				{ status:'theirs', x:10, y:8 },
+				{ status:'theirs', x:11, y:8 },
+				{ status:'empty', x:12, y:8 },
+				{ status:'theirs', x:13, y:8 },
+				{ status:'empty', x:14, y:8 },
+				{ status:'empty', x:15, y:8 }
+			]
+		};
+	}
+	
+	getCanvas = () => {
+		var canvas = {};
+		canvas.width = document.getElementById('container').offsetWidth;
+		canvas.height = (canvas.width/16)*9;
+		canvas.circleSize = canvas.width / 20;
+		canvas.circleOffset = canvas.width / 16;
+		
+		return canvas;	
 	};
+	
+	updateCanvas = () => {
+		this.setState({
+			canvas: this.getCanvas()
+		});
+	};
+	
+	componentDidMount() {
+		window.addEventListener("resize", this.updateCanvas.bind(this));
+	}
+	
+	componentWillUnmount() {
+		window.removeEventListener("resize", this.updateCanvas.bind(this));
+	}
 	
 	countMine = () => {
 		var count = 0;
@@ -282,9 +212,8 @@ class App extends React.Component {
 	};
 	
 	// when a circle is clicked
-	handleCircleClick = (gridCircle) => {
+	handleCircleClick = (key) => {
 		var circles = this.state.circles;
-		var key = gridCircle.props.id;
 		
 		// if the circle is not already taken by someone else
 		if(circles[key].status !== 'theirs') {
@@ -297,7 +226,12 @@ class App extends React.Component {
 					// if the circle was empty, make it mine
 					circles[key].status = 'mine';
 				} else {
-					alert('Whoa there! Let\'s not get greedy, now.');
+					this.toastr.warning(
+						null,
+						"Whoa there! Let's not get greedy, now.", {
+						timeOut: 3000,
+						extendedTimeOut: 3000
+				    });
 					return;
 				}
 			}
@@ -306,7 +240,12 @@ class App extends React.Component {
 				circles: circles
 			});
 		} else {
-			alert('Looks like someone else has already staked a claim this circle.');
+			this.toastr.warning(
+				null,
+				"Looks like someone else has already staked a claim this circle.", {
+				timeOut: 3000,
+				extendedTimeOut: 3000
+		    });
 		}
 	}
 	
@@ -314,18 +253,20 @@ class App extends React.Component {
 	render() {
 		// loop over circles array and add each to the canvas
 		var circles = this.state.circles.map(function(circle,key) {
-			return <GridCircle key={key} id={key} status={circle.status} x={circle.x*50} y={circle.y*50} handleClick={this.handleCircleClick} />;
+			return <GridCircle key={key} id={key} status={circle.status} x={circle.x*this.state.canvas.circleOffset} y={circle.y*this.state.canvas.circleOffset} width={this.state.canvas.circleSize} height={this.state.canvas.circleSize} handleClick={this.handleCircleClick} />;
 		}.bind(this));
 		
 		// output canvas
 		return (
-			<div>
-				<Stage width={1300} height={500}>
+			<div className="row">		
+				<Stage width={this.state.canvas.width} height={this.state.canvas.height}>
 					<Layer>
+						<Rect x={0} y={0} width={this.state.canvas.width} height={this.state.canvas.height} fill={'#f5f5f5'} />
 						{circles}
 					</Layer>
-				</Stage>
+				</Stage>		
 				<Counter count={this.countMine()} />
+				<ToastContainer ref={(input) => {this.toastr = input;}} toastMessageFactory={ToastMessageFactory} className={"toast-top-right"} />
 			</div>
 		);
 	}
